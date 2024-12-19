295ab9b63503d3cf906c3ffe06f729315402b455
GeorgeC <gcolon021@gmail.com>
2024-12-18 20:32:24 -0500

    Add SQL script to create dictionary API resource

---
+++ Baseline/picsure/V3__CREATE_DICTIONARY_RESOURCE.sql
@@ -0,0 +1,7 @@
+SET @dictResourceUUID = unhex(REPLACE(UUID(), '-', ''));
+
+INSERT INTO `resource`
+(uuid, targetURL, resourceRSPath, description, name, token, hidden, metadata)
+VALUES
+    (@dictResourceUUID, NULL, 'http://dictionary-api/',
+     'Dictionary API', 'dictionary-api', NULL, TRUE, NULL);
