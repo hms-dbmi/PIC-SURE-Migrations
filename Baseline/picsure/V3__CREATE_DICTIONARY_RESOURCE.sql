557352abb29c42dc26f43019aa2f5ac7127f0e71
Luke Sikina <lucas.skina@gmail.com>
2025-05-19 10:56:28 -0400

    [CHORE] Fix dictionary migrations
    - Remove dictionary resource migrations. This is done in the pic-sure
      repo
    - Move the dump resource migrations to the GIC folders
    - Add permissions to query dictionary in GIC CA

--- Baseline/picsure/V3__CREATE_DICTIONARY_RESOURCE.sql
+++ Baseline/picsure/V3__CREATE_DICTIONARY_RESOURCE.sql
@@ -1,7 +1,9 @@
 SET @dictResourceUUID = unhex(REPLACE(UUID(), '-', ''));
+--
+--INSERT INTO `resource`
+--(uuid, targetURL, resourceRSPath, description, name, token, hidden, metadata)
+--VALUES
+--    (@dictResourceUUID, NULL, 'http://dictionary-api/',
+--     'Dictionary API', 'dictionary-api', NULL, TRUE, NULL);
 
-INSERT INTO `resource`
-(uuid, targetURL, resourceRSPath, description, name, token, hidden, metadata)
-VALUES
-    (@dictResourceUUID, NULL, 'http://dictionary-api/',
-     'Dictionary API', 'dictionary-api', NULL, TRUE, NULL);
+-- Bad migrator, duplicate of migrator in main PIC-SURE repo
