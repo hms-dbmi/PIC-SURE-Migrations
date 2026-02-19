6ac0e76681267fefb670775c61e1b0d7f8172125
James <Jamestp19@gmail.com>
2026-02-19 11:30:24 -0500

    Update for proxy use

--- Baseline/picsure/V4__CREATE_LOGGING_RESOURCE.sql
+++ Baseline/picsure/V4__CREATE_LOGGING_RESOURCE.sql
@@ -3,4 +3,4 @@ INSERT INTO `resource`
     (uuid, targetURL, resourceRSPath, description, name, token, hidden, metadata)
     VALUES
     (unhex(REPLACE('35d190ad-6547-46c5-a7ad-ae35cfd2e34b', '-', '')), NULL,
-     'http://pic-sure-logging:8080/', 'Logging Service', 'logging', NULL, TRUE, NULL);
+     'http://pic-sure-logging/', 'Logging Service', 'pic-sure-logging', NULL, TRUE, NULL);
