14e749e934d50c7424a40aa93668b5dc091fb651
James <Jamestp19@gmail.com>
2026-04-14 21:13:56 -0400

    [ALS-11186] Add Logging Resource (#9)

---
+++ GIC-Common-Area/picsure/V4__CREATE_LOGGING_RESOURCE.sql
@@ -0,0 +1,6 @@
+use picsure;
+INSERT INTO `resource`
+    (uuid, targetURL, resourceRSPath, description, name, token, hidden, metadata)
+    VALUES
+    (unhex(REPLACE('35d190ad-6547-46c5-a7ad-ae35cfd2e34b', '-', '')), NULL,
+     'http://pic-sure-logging/', 'Logging Service', 'pic-sure-logging', NULL, TRUE, NULL);
